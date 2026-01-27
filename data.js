221112d369edf6ef751481ba6731801f45b248d5
Ash571-bit <ashusinghh5711@gmail.com>
2026-01-27 18:54:38 +0530

    Fix comment typo in students array

--- data.js
+++ data.js
@@ -5,13 +5,14 @@
 
 const students = [
     
-    // 1. Copy the line below
+    // 1. Copy the line bel
     { name: "Puneet Tiwari", username: "puneet426", thought: "Let's build something awesome!" },
     { name: "Ayush Kumar", username: "kansyakarayush-tech", thought: "Let's build something awesome!" },
    { name: "shreyansh gopal", username: "Gopal1225", thought: "Let's build something awesome!" },
    { name: "Tuhin Bhattacharjee", username: "bhattacharjeetuhin190-ops", thought: "Let's Go" },
     // 2. Paste it here and change the details
     { name: "Mahak Yadav", username: "mahak2211", thought: "Let's build something awesome!" },
-    
+
+   { name: "ashu singh", username: "ASH571-bit", thought: "Let's build something awesome!" }
     
 ];
