37481d2b761cf09120ce5ec01ee0bb9b54318a82
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2026-01-27 21:51:07 +0530

    Update data.js

--- data.js
+++ data.js
@@ -9,11 +9,13 @@ const students = [
    
     { name: "Ayush Kumar", username: "kansyakarayush-tech", thought: "Let's build something awesome!" },
    { name: "shreyansh gopal", username: "Gopal1225", thought: "Let's build something awesome!" },
+    { name: "khushi", username: "khushi@5022008", thought: "Let's build something awesome!" },
+   { name: "Mahak Yadav", username: "mahak2211", thought: "Let's build something awesome!" },
    { name: "Tuhin Bhattacharjee", username: "bhattacharjeetuhin190-ops", thought: "Let's Go" },
     // 2. Paste it here and change the details
-    { name: "Mahak Yadav", username: "mahak2211", thought: "Let's build something awesome!" },
+    
 
    { name: "ashu singh", username: "ASH571-bit", thought: "Let's build something awesome!" },
-    { name: "khushi", username: "khushi@5022008", thought: "Let's build something awesome!" },
+   
     
 ];
