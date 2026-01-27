55f96ee1b73d53b05f6d281f77f73fde724a16fe
Puneet Tiwari <tiwaripuneet426@gmail.com>
2026-01-27 17:58:17 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,15 @@
+/* Welcome to ForgeX! 
+   To add your card, copy the example line and paste it at the bottom.
+   Change the Name, Username, and Thought. 
+*/
+
+const students = [
+    
+    // 1. Copy the line below
+    { name: "Puneet Tiwari", username: "puneet426", thought: "Let's build something awesome!" },
+    
+
+    // 2. Paste it here and change the details
+    
+    
+];
